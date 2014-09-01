b95b75bbc2f4b91e4ddfdd75deff14add609082d
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-09-01 13:28:09 +0200

    upgrade to rails 4.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,34 +5,15 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_generic.
 
+load File.expand_path('../../app_root.rb', __FILE__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
-def init_wagon
-  init_app_helper
-  config_rspec
-end
+require File.join(ENV['APP_ROOT'], 'spec', 'spec_helper.rb')
 
-def init_app_helper
-  load File.expand_path('../../app_root.rb', __FILE__)
-  ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[HitobitoGeneric::Wagon.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
-  require File.join(ENV['APP_ROOT'], 'spec', 'spec_helper.rb')
-
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[HitobitoGeneric::Wagon.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
-end
-
-def config_rspec
-  RSpec.configure do |config|
-    config.fixture_path = File.expand_path('../fixtures', __FILE__)
-  end
-end
-
-
-if /spork/i =~ $PROGRAM_NAME || (RSpec.respond_to?(:configuration) && RSpec.configuration.drb?)
-  require 'spork'
-  Spork.prefork { init_wagon }
-  Spork.each_run {}
-else
-  init_wagon
+RSpec.configure do |config|
+  config.fixture_path = File.expand_path('../fixtures', __FILE__)
 end
