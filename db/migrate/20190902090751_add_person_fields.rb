50abc6fd275e9daa289a9954ed1e0d3dae2e79a7
Andreas Maierhofer <a.maierhofer@gmail.com>
2020-01-14 14:38:17 +0100

    Specify Migration version on each migration

--- db/migrate/20190902090751_add_person_fields.rb
+++ db/migrate/20190902090751_add_person_fields.rb
@@ -5,7 +5,7 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_generic.
 
-class AddPersonFields < ActiveRecord::Migration
+class AddPersonFields < ActiveRecord::Migration[4.2]
   def change
     add_column(:people, :title, :string)
     add_column(:people, :nationality, :string)
